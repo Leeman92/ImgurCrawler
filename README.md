1d2615980bff246f55cb7fe0c691a05eaf72412b
Leeman <contact@leeman.com.de>
2014-09-05 01:48:57 +0200

    Initial Commit

--- README.md
+++ README.md
@@ -1,4 +1,15 @@
-ImgurCrawler
+Imgur Crawler
 ============
 
 First Python project. Imgur Crawler to download "albums/gallerys" [not sure what to exactly call them] if there's no Download All button. Like for example at imgur.com/r/Girls_smiling
+
+Known Issues
+============
+
+* Only downloads first few, initally loaded pictures even though there are more
+* Counts as downloaded picture even though it did not downloaded one
+
+Features
+=========
+Easy download for big "albums/gallerys" (not sure what they are called in imgur). Names of pictures all lowercase. Spaces replaced with _
+and no double download [Goes by Name though]
---
+++ main.py
@@ -0,0 +1,58 @@
+from urllib import request
+from os import path
+
+from bs4 import BeautifulSoup
+import requests
+
+
+skipping = 0
+base_url = "http://imgur.com"
+sub_url = ""  # link to the album you want to download. example /r/Girls_smiling
+
+
+def web_spider(max_pictures, start_picture=1):
+    pictures = 1
+    global skipping
+    url = base_url + sub_url
+    source_code = requests.get(url)
+    # just get the code, no headers or anything
+    plain_text = source_code.text
+    # BeautifulSoup objects can be sorted through easy
+    soup = BeautifulSoup(plain_text)
+    for link in soup.findAll('a', {'class': 'image-list-link'}):
+        if pictures <= max_pictures:
+            href = link.get('href')
+            # print(href)
+            if pictures < start_picture:
+                pictures += 1
+            elif skipping == 1:
+                skipping = 0
+            else:
+                download_picture(base_url + href, "")
+                pictures += 1
+                left = max_pictures - pictures
+            print("There are %s pictures left to download!" % left)
+
+
+def download_picture(url, name):
+    source_code = requests.get(url)
+    plain_text = source_code.text
+    soup = BeautifulSoup(plain_text)
+    global skipping
+
+    for title in soup.findAll('h2', {'id': 'image-title'}):
+        name = title.string
+    for image in soup.findAll('link', {'rel': 'image_src'}):
+        image_source = image.get('href')
+        filename = name.replace(',', '_').replace(' ', '_').lower().replace('?', '') + str(image_source)[-4:]
+        if path.isfile(filename):
+            skipping = 1
+            print("Skipping.. Already downloaded...")
+            continue
+        if str(image_source).endswith(".jpg") or str(image_source).endswith(".JPG") or str(image_source).endswith(
+                ".gif"):
+            request.urlretrieve(image_source, filename)
+            print("Successfully downloaded", filename)
+
+
+web_spider(start_picture=1, max_pictures=100)
