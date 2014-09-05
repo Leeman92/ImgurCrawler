53202dddbc5d6ef36f13a00241e1243597abe8e5
Leeman <contact@leeman.com.de>
2014-09-05 03:40:27 +0200

    Fixed issue with skipping & countng how many pictures left to download

--- imgurCrawler/main.py
+++ imgurCrawler/main.py
@@ -6,7 +6,6 @@ from bs4 import BeautifulSoup
 import requests
 
 
-skipping = 0
 base_url = "http://imgur.com"
 # link to the album you want to download. example /r/Girls_smiling
 sub_url = input("Please define where to download the pictures from: ")
@@ -18,7 +17,6 @@ if sub_url.startswith(base_url):
 
 def web_spider(max_pictures):
     global pictures
-    global skipping
     url = base_url + sub_url
     source_code = requests.get(url)
     # just get the code, no headers or anything
@@ -28,9 +26,6 @@ def web_spider(max_pictures):
     for link in soup.findAll('a', {'class': 'image-list-link'}):
         href = link.get('href')
         if pictures <= max_pictures:
-            if skipping == 1:
-                skipping = 0
-                pictures -= 1
             download_picture(base_url + href, max_pictures)
     print("No more pictures to download on this site")
 
@@ -39,16 +34,15 @@ def download_picture(url, max_pictures):
     source_code = requests.get(url)
     plain_text = source_code.text
     soup = BeautifulSoup(plain_text)
-    global skipping
     global pictures
 
     for title in soup.findAll('h2', {'id': 'image-title'}):
         name = title.string
     for image in soup.findAll('link', {'rel': 'image_src'}):
         image_source = image.get('href')
-        filename = name.replace(',', '_').replace(' ', '_').lower().replace('?', '').replace("\"", "").replace("/", "") + str(image_source)[-4:]
+        filename = name.replace(',', '_').replace(' ', '_').lower().replace('?', '').replace(":", "").replace("\"", "")\
+                       .replace("/", "") + str(image_source)[-4:]
         if path.isfile(filename):
-            skipping = 1
             print("Skipping.. Already downloaded...")
             continue
         elif str(image_source).endswith(".jpg") or str(image_source).endswith(".JPG") or str(image_source).endswith(
